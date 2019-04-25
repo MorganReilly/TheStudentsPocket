1416cacdca6aafcff562238adf3c6cf7d76643e0
Cathal Butler <cathalbutler@outlook.com>
2019-04-25 21:49:38 +0100

    Testing files added.

--- TheStudentsPocket/src/app/timetable/timetable.page.ts
+++ TheStudentsPocket/src/app/timetable/timetable.page.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../services/api.service';
-import set = Reflect.set;
-import {not} from 'rxjs/internal-compatibility';
 
 @Component({
     selector: 'app-timetable',
