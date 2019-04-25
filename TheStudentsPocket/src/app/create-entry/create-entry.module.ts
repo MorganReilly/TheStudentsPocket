acad0aecc919f49bb4186d3548d8c50854dc520d
Morgan <G00303598@gmit.ie>
2019-04-25 01:11:50 +0100

    Some minor UI updates

--- TheStudentsPocket/src/app/create-entry/create-entry.module.ts
+++ TheStudentsPocket/src/app/create-entry/create-entry.module.ts
@@ -1,26 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { CreateEntryPage } from './create-entry.page';
+import {CreateEntryPage} from './create-entry.page';
+import {MatDialogModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: CreateEntryPage
-  }
+    {
+        path: '',
+        component: CreateEntryPage
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [CreateEntryPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes),
+        MatFormFieldModule,
+        MatDialogModule,
+        MatInputModule,
+    ],
+    declarations: [CreateEntryPage]
 })
-export class CreateEntryPageModule {}
+export class CreateEntryPageModule {
+}
